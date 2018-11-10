d7304fd6924c45d2747c28b58418d21bd71665a0
Michael Sobral <AKuma@Michaels-iMac.local>
2018-11-10 18:24:44 +0000

    End of Section 6

--- TCWages-SPA/src/app/nav/nav.component.ts
+++ TCWages-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,27 +10,31 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('Logged in!');
       console.log('Logged in succesfully!');
     }, error => {
-      console.log('Failed to login');
+      // console.log('Failed to login');
+      this.alertify.error(error);
       console.log(error);
     });
 
   }
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
+    this.alertify.message('Logged Out!');
     console.log('Logged Out');
   }
 
