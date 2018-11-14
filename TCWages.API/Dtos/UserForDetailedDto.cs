2fdc73032f772802afb1ec6700b01c7c1a40dbd6
Michael Sobral <AKuma@Michaels-iMac.local>
2018-11-14 12:12:17 +0000

    End of Section 9

--- TCWages.API/Dtos/UserForDetailedDto.cs
+++ TCWages.API/Dtos/UserForDetailedDto.cs
@@ -8,6 +8,7 @@ namespace TCWages.API.Dtos
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        public string KnownAs { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
         public DateTime DateOfBirth { get; set; }
