79588c71cba99a3ebee1747e49eceaf11a5aa62d
Michael Sobral <AKuma@Michaels-iMac.local>
2019-01-05 13:02:22 +0000

    Update Angular dependecies

--- TCWages-SPA/src/app/app.module.ts
+++ TCWages-SPA/src/app/app.module.ts
@@ -30,7 +30,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
-import {TimeAgoPipe} from 'time-ago-pipe';
+import { TimeAgoPipe } from 'time-ago-pipe';
 import { ListsResolver } from './_resolvers/lists.resolver';
 
 export function tokenGetter() {
